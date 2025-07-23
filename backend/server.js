e73573dec662b29fa1ddf00893366bc3bbd55a84
shaho-a <shahoalirezaei95@gmail.com>
2025-07-23 20:01:59 +0330

    Add start script for Railway deploy and edit static port

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,7 @@ app.use("/api/setup", setupRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/admins", adminsRouter);
 
-app.listen(8001, () => console.log("Server running on port 8001"));
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
