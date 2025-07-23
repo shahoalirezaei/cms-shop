c62ccde1c5ef4400cbd5d3a484a2ab1176099402
shaho-a <shahoalirezaei95@gmail.com>
2025-07-23 21:39:07 +0330

    .env and server.js chanched

--- backend/db/CmsShop.js
+++ backend/db/CmsShop.js
@@ -2,10 +2,11 @@ const mysql = require("mysql");
 const util = require("util");
 
 const CmsShopDB = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cms_store",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 // تبدیل متد query به Promise-based
