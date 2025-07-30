e0e91a2841f128b056f80c349a7b40433c8fdd3f
shaho-a <shahoalirezaei95@gmail.com>
2025-07-30 22:22:47 +0330

    import API_ENDPOINTS in newuserjoined and lastorders

--- cms-store/src/components/lastOrders/LastOrdersTable.jsx
+++ cms-store/src/components/lastOrders/LastOrdersTable.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ErrorBox from "../errorBox/ErrorBox";
+import { API_ENDPOINTS } from "../../config/api";
 import {
   FaEdit,
   FaInfoCircle,
--- cms-store/src/components/newUserJoinedTable/NewUserJoinedTable.jsx
+++ cms-store/src/components/newUserJoinedTable/NewUserJoinedTable.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ErrorBox from "../errorBox/ErrorBox";
+import { API_ENDPOINTS } from "../../config/api";
 import {
   FaEdit,
   FaInfoCircle,
