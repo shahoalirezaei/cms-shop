fd8f7d233936db26e227a07ef5e7b73e0ddda19f
shaho-a <shahoalirezaei95@gmail.com>
2025-07-31 17:47:31 +0330

    fixed: authfetch in front

--- cms-store/src/hooks/useAuthFetch.js
+++ cms-store/src/hooks/useAuthFetch.js
@@ -18,19 +18,60 @@ export const useAuthFetch = () => {
         body: body ? JSON.stringify(body) : null,
       });
 
-      const data = await response.json();
+      let data = null;
+
+      // فقط در صورتی که بدنه داشته باشه بخون
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        data = await response.json();
+      }
 
       if (!response.ok) {
-        throw new Error(data.message || "Something went wrong");
+        throw new Error(data?.message || "Something went wrong");
       }
 
       return { error: null, data };
     } catch (error) {
       return { error, data: null };
     } finally {
-      setLoading(false); // در پایان، چه موفق چه خطا
+      setLoading(false);
     }
   };
 
   return { authFetch };
 };
+
+
+// export const useAuthFetch = () => {
+//   const { setLoading } = useContext(LoadingContext);
+
+//   const authFetch = async (url, method = "GET", body = null) => {
+//     setLoading(true);
+//     const headers = {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${localStorage.getItem("token")}`,
+//     };
+
+//     try {
+//       const response = await fetch(url, {
+//         method,
+//         headers,
+//         body: body ? JSON.stringify(body) : null,
+//       });
+
+//       const data = await response.json();
+
+//       if (!response.ok) {
+//         throw new Error(data.message || "Something went wrong");
+//       }
+
+//       return { error: null, data };
+//     } catch (error) {
+//       return { error, data: null };
+//     } finally {
+//       setLoading(false); // در پایان، چه موفق چه خطا
+//     }
+//   };
+
+//   return { authFetch };
+// };
