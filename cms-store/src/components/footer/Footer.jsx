986dcbbfa5ef65adb40e7c39ede0575c0f19df37
shaho-a <shahoalirezaei95@gmail.com>
2025-08-02 20:39:26 +0330

    change footer style

--- cms-store/src/components/footer/Footer.jsx
+++ cms-store/src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full mt-auto z-50 text-center text-sm text-gray-400 py-4 border-t  bg-white dark:bg-gray-900">
+    <footer className="w-full mt-auto z-5 text-center text-sm text-gray-400 py-4 border-t  bg-white dark:bg-gray-900">
       <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-6 px-4">
         <span>© {new Date().getFullYear()} CMS-Store — Developed by <strong className="text-gray-600 dark:text-white">Shaho Alirezaei</strong></span>
         <span>📧 shahoalirezaei95@gmail.com</span>
